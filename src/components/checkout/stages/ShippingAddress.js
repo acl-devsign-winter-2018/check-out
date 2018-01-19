bcbefe7533758bc5db8ba7ac6bb720e13ae2ce10
G.Provost <grace.g.provost@gmail.com>
2018-01-19 12:48:33 -0800

    start shipping address functionality

--- src/components/checkout/stages/ShippingAddress.js
+++ src/components/checkout/stages/ShippingAddress.js
@@ -6,9 +6,19 @@ const template = new Template(html);
 
 export default class ShippingAddress {
 
+  handleSubmit(target) {
+    
+  }
+
   render() {
     const dom = template.clone();
 
+    const form = dom.querySelector('form');
+
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      this.handleSubmit(event.target);
+    });
    
     return dom;
   }
