68af05ced8610fa87d212032a70d62a70d471cae
G.Provost <grace.g.provost@gmail.com>
2018-01-19 10:14:45 -0800

    set up. Create login page.

---
+++ src/components/checkout/Login.js
@@ -0,0 +1,57 @@
+import html from './login.html';
+import './login.css';
+import Template from '../Template';
+
+const template = new Template(html);
+
+export default class Login {
+
+  handleSubmit(form) {
+    const data = new FormData(form);
+    const obj = {};
+    data.forEach((value, key) => obj[key] = value);
+    console.log('would have submitted', obj);
+  }
+
+  render() {
+    const dom = template.clone();
+
+    const form = dom.querySelector('form');
+
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      this.handleSubmit(event.target);
+    });
+
+    form.addEventListener('blur', event => {
+      const element = event.srcElement;
+      if(element.type === 'submit') return;
+      element.nextElementSibling.textContent = element.validationMessage;
+
+      const valid = form.checkValidity();
+      if(!valid) {
+        this.submit.setAttribute('disabled', 'true');
+      } else {
+        this.submit.removeAttribute('disabled');
+      }
+    }, true);
+
+    this.submit = dom.querySelector('button[type=submit]');
+
+    const password = dom.querySelector('input[type=password');
+
+    password.addEventListener('keyup', event => {
+      const target = event.target;
+      target.setCustomValidity('');
+      if(!target.checkValidity()) return;
+
+      const valid = target.value.toLowerCase() !== target.value;
+      if(!valid) {
+        target.setCustomValidity('Password must contain at least one uppercase character');
+      }
+    });
+
+    return dom;
+  } //render
+
+}
