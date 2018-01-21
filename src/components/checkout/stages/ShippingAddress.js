024fdff00f94107bce7dbbeafdcbb09b749f29b9
G.Provost <grace.g.provost@gmail.com>
2018-01-21 12:59:40 -0800

    fix city and state validation.

--- src/components/checkout/stages/ShippingAddress.js
+++ src/components/checkout/stages/ShippingAddress.js
@@ -7,23 +7,37 @@ const template = new Template(html);
 export default class ShippingAddress {
 
   handleSubmit(target) {
-    const data = new FormData(form);
+    const data = new FormData(target);
     const obj = {};
     data.forEach((value, key) => obj[key] = value);
     console.log('would have submitted', obj);
+  }
+
+  checkCityValidity(element) { 
+    if(element.checkValidity()) { // if the entry is valid
+      element.nextElementSibling.textContent = '';
+    } else if(element.value !== '' && !element.checkValidity()) { //if the value is not empty and invalid
+      element.nextElementSibling.textContent = 'No numbers or special characters allowed';
+    } else if(element.value === '') { // if the entry empty
+      element.nextElementSibling.textContent = element.validationMessage;
+    }
+  }
+
+  checkOptionValidity(element) {
+    if(element.options[element.selectedIndex].text === 'Select') {
+      element.nextElementSibling.textContent = 'Please select a state / providence';
+    } else if(element.options[element.selectedIndex].text !== 'Select') {
+      element.nextElementSibling.textContent = '';
+    }
   }
 
   render() {
     const dom = template.clone();
 
     const form = dom.querySelector('form');
-    const city = dom.querySelector('#city');
-    city.oninvalid = function(event) {
-      event.target.setCustomValidity('No numbers or special characters allowed');
-    }
-
+    
     this.submit = dom.querySelector('button[type=submit]');
-
+    
     form.addEventListener('submit', event => {
       event.preventDefault();
       this.handleSubmit(event.target);
@@ -33,14 +47,16 @@ export default class ShippingAddress {
       const element = event.srcElement;
       if(element.type === 'submit') return;
       if(element.type === 'checkbox') return; //fix clear of checkbox label
+      
       if(element.options){ //Checks that user selected a state
-        if(element.options[element.selectedIndex].text === 'Select') {
-          element.nextElementSibling.textContent = 'Please select a state / providence';
-        }
-        if(element.options[element.selectedIndex].text !== 'Select') {
-          element.nextElementSibling.textContent = '';
-        }
-      } else {
+        this.checkOptionValidity(element);
+      } 
+
+      if(element.id === 'city') {
+        this.checkCityValidity(element);
+      }
+
+      if(element.id !== 'city' && !element.options) {
         element.nextElementSibling.textContent = element.validationMessage;
       }
 
